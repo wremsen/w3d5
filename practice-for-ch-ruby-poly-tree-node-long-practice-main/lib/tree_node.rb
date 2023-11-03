dda3f7f193508d041d6da7749773afe8746008be
wremsen <wcremsen@gmail.com>
2023-11-03 12:29:54 -0400

    add partial #parent=

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,18 @@
 class PolyTreeNode
+
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        @parent.children << self
+    end
+
+
   
 end
