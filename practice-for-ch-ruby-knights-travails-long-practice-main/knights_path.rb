7e206e7b278a8ebd7f0ef70954365e335d011249
JhonJSC182 <jhonjsc182@hotmail.com>
2023-11-06 13:18:56 -0500

    added class method and fix moves

--- practice-for-ch-ruby-knights-travails-long-practice-main/knights_path.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knights_path.rb
@@ -2,12 +2,26 @@ require_relative '../practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/
 
 class KnightPathFinder
 
-    def valid_moves(pos)
-        moves = []
-        spaces = [0, 1, 2, 3, 4, 5, 6, 7]
+    MOVES = [
+        [2,1], [2,-1], 
+        [-2,1], [-2,-1], 
+        [1,2], [-1,2], 
+        [1,-2],[-1,-2]
+    ]
+
+    def self.valid_moves(pos)
+        v_moves = []
 
         row = pos[0]
         col = pos[1]
+        MOVES.each do |ele|
+            new_row = row + ele[0]
+            new_col = col + ele[1]
+            if (new_row >= 0 && new_row < 8) && (new_col >= 0 && new_col < 8)
+                v_moves << [new_row, new_col]
+            end
+        end
+        v_moves
     end
 
     attr_reader :root_node
@@ -23,6 +37,12 @@ class KnightPathFinder
     def new_move_positions(pos)
         moves = KnightPathFinder.valid_moves(pos)
         moves.reject { |ele| @considered_positions.include?(ele) }
+
+        moves.each do |ele|
+            @considered_positions << ele
+        end
+
+        return moves
     end
 
 
